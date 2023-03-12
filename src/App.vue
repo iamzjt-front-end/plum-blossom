96cfa5042765d4ca79cdd9f76e07dffbb0d18ca2
iamzjt-front-end <iamzjt@foxmail.com>
2023-03-13 06:26:35 +0800

    :sparkles: feat: 引入svg

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,18 @@
 
 <template>
   <div>
+    <svg-icon name="plum" size="5em" />
+    <h1>梅开二度</h1>
   </div>
 </template>
 
 <style lang="scss" scoped>
+div {
+  display: flex;
+  align-items: center;
 
+  :deep(.svg-icon) {
+    margin-right: 12px;
+  }
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import 'virtual:svg-icons-register';
+import SvgIcon from './components/SvgIcon.vue';
 
-createApp(App).mount('#app')
+createApp(App)
+	.component('svg-icon', SvgIcon)
+	.mount('#app');
